3649cf43453ccd1207048ed8b1a4eed4acac36c9
snehalshinde324 <72022408+snehalshinde324@users.noreply.github.com>
2020-11-30 15:07:07 +0530

    Add files via upload

---
+++ datastructure/Bracket.java
@@ -0,0 +1,51 @@
+package datastructure;
+
+class A {
+
+	  
+		   static int countopen=0;
+		   static int countclose=0;
+		    void push(char i)
+		    {
+		        if(i=='(' || i=='{' || i=='[')
+		        {
+		       countopen++;
+		       
+		        }
+		        
+		     
+		    }
+		    void pop(char i)
+		    {
+		        if(i==')' || i=='}' || i==']')
+		        {
+		           countclose++;
+		        }
+		    }
+		}
+	public 	class Bracket
+		{
+		    public static void main(String[] args)
+		    {
+		    	A obj=new A();
+		        String s=("{[()]}");
+		        
+		       for (int i=0; i<s.length();i++)
+		       {
+		           obj.push(s.charAt(i));
+		       }
+		     
+		       for (int i=0; i<s.length();i++)
+		       {
+		           obj.pop(s.charAt(i));
+		       }
+		       if(A.countopen==A.countclose)
+		       {
+		           System.out.println("blanced");
+		       }
+		       else
+		       {
+		           System.out.println("unbalnced");
+		       }
+		    }
+		}
